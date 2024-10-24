f3aedbc43f1554525363a19fb491cef7440710ff
KimberlyDL <1d3.de.leon.kimberly.nicole.i@gmail.com>
2024-10-25 06:35:49 +0800

    back-up copy

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from './router';
+import vuetify from './plugins/vuetify';
+import '@/scss/style.scss';
+import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar';
+import VueApexCharts from 'vue3-apexcharts';
+import VueTablerIcons from 'vue-tabler-icons';
+import Maska from 'maska';
+const app = createApp(App);
+app.use(router);
+app.use(PerfectScrollbarPlugin);
+app.use(VueTablerIcons);
+app.use(Maska);
+app.use(VueApexCharts);
+app.use(vuetify).mount('#app');
